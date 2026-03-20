e4d640a3610f6b13d5a4f63b27a561b7977a8665
Prashant Goel <pgoel147@gmail.com>
2026-03-21 03:20:05 +0530

    Initial commit - First TestNG test with Maven

---
+++ src/test/java/com/prashant/automation/tests/MyFirstTest.java
@@ -0,0 +1,58 @@
+package com.prashant.automation.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class MyFirstTest {
+	
+	@BeforeMethod
+	public void setUp() {
+		System.out.println("--- Test Starting ---");
+		
+	}
+	
+	@Test 
+	public void verifyAddition() {
+		System.out.println("Running: verifyAddition");
+		int result = 2 + 2;
+		Assert.assertEquals(result, 4, "Addition Should be 4");
+		
+	}
+	
+	@Test
+	public void verifyMyName() {
+		System.out.println("Running: Verify MyName");
+		String name = "Prashant";
+		Assert.assertEquals(name,  "Prashant", "Name should match");
+		
+	}
+	
+	@Test 
+	public void tearDown() {
+		System.out.println("--- Test Finished ---");
+		
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
